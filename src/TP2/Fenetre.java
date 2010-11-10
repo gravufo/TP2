22237a6e1fff8f1d1694df5cb40cd19c292d9169
Gravufo <gravufo@gmail.com>
2010-11-10 14:58:18 -0500

    Added Fenetre.java

---
+++ src/TP2/Fenetre.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TP2;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Gravufo
+ */
+public class Fenetre extends JFrame
+{
+    public Fenetre()
+    {
+        super();
+        this.setSize(600, 600);
+        this.setVisible(true);
+    }
+}
