b2054755b696ea521b7afd3eab074ff5d3859a2a
Gravufo <gravufo@gmail.com>
2010-11-10 17:59:42 -0500

    added new files to display the grid

---
+++ src/TP2/Fenetre.java
@@ -0,0 +1,33 @@
+package TP2;
+
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gravufo
+ */
+public class Fenetre extends JFrame
+{
+    private Damier damier;
+    public Fenetre()
+    {
+        super("Super damier de Christian!");
+
+        damier = new Damier();
+
+        this.setLayout(new GridLayout(1,0));
+        this.setPreferredSize(new Dimension(600,600));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.add(damier);
+        this.pack();
+        this.setVisible(true);
+    }
+}
