4738598d1333276428a472e38ad8a7754d5d9657
Gravufo <gravufo@gmail.com>
2010-11-10 12:29:43 -0500

    added main, test

---
+++ src/TP2/main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TP2;
+
+/**
+ *
+ * @author Gravufo
+ */
+public class main
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+
+}
